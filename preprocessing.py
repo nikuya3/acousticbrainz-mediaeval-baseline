b28a47ab51bcbecc9fab8da46ad1dc4bb1cbe38c
nikuya3 <54780682+nikuya3@users.noreply.github.com>
2020-01-11 17:40:34 +0100

    Fix preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,14 @@
 from csv import reader
 from json import load
 from math import sqrt
-from multiprocessing import Array, Process, BoundedSemaphore, Queue
+from multiprocessing import Array, Process, Queue
+from glob import glob
 from os import makedirs
 
 import numpy as np
+import pandas as pd
 
-datasets = ["tagtraum", "discogs", "allmusic", "lastfm"]
+datasets = ["allmusic", "tagtraum", "discogs", "lastfm"]
 modes = ["train", "validation"]
 categorical_features = ["key_key", "key_scale", "chords_key", "chords_scale"]
 categorical_levels = {"key_key": ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"],
@@ -31,34 +33,32 @@ def get_nested_dict_values(d):
         yield d
 
 
-def process_observation(row, mode, dataset, sum_x, sum_x2, sema, queue):
+def process_observation(row, mode, dataset, sum_x, sum_x2, queue):
     mbid = row[0]
     genres = row[2:]
     with open(f"{data_dir}/acousticbrainz-mediaeval-{mode}/{mbid[:2]}/{mbid}.json", "r") as file:
-        print(f"{data_dir}/acousticbrainz-mediaeval-{mode}/{mbid[:2]}/{mbid}.json")
         observation = load(file)
     observation.pop("metadata", None)  # metadata contains non-numeric data which is largely not present in the test set
     observation["rhythm"].pop("beats_position", None)  # beats_position seems to be removed, probably because it is of variable length
-    observation["rhythm"].pop("beats_count", None)
+    observation["rhythm"]["beats_loudness"].pop("median", None) # beats_loudness*.median is missing in some observations
+    observation["rhythm"]["beats_loudness_band_ratio"].pop("median", None)
     features = list(get_nested_dict_values(observation))
     # -- one hot encode all remaining categorical values inplace
     for category in categorical_levels:
         feature_value = observation["tonal"][category]
         category_length = len(categorical_levels[category])
         level_ix = categorical_levels[category].index(feature_value)
-        one_hot_encoded = np.eye(1, M=category_length - 1, k=level_ix)[0]  # dummy trap
+        one_hot_encoded = np.eye(1, M=category_length, k=level_ix)[0]
         feature_ix = features.index(feature_value)
         features.pop(feature_ix)
         features[feature_ix:feature_ix] = one_hot_encoded
     features.insert(0, mbid)  # insert observation id in resulting feature set
     genres_encoded = [all_genres.index(genre) for genre in genres if genre in all_genres]
     genres_encoded.insert(0, mbid)
-    # --
     for i in range(len(features) - 1):
         sum_x[i] += features[i + 1]
         sum_x2[i] += features[i + 1] ** 2
     queue.put((mode, dataset, str(features)[1:-1] + "\n", str(genres_encoded)[1:-1] + "\n"))
-    sema.release()
 
 
 def write_output(queue):
@@ -79,27 +79,29 @@ def write_output(queue):
 
 def main():
     pool = []
-    features_dim = 2670
+    features_dim = 2669
     # -- sum_x and sum_x2 are the cumulative sum (of squares) of all features. these values are needed to compute the
     # -- mean and standard deviation in a memory-efficient manner after all observations have been passed
     sum_x = Array('d', features_dim)
     sum_x2 = Array('d', features_dim)
-
     n = 0
     output_queue = Queue()
-    concurrent_processes = 4
-    semaphore = BoundedSemaphore(concurrent_processes)
+    concurrent_processes = 24
     consumer = Process(target=write_output, args=(output_queue,))
     consumer.start()
-    for dataset in datasets[:1]:
-        for mode in modes[:1]:
+    for dataset in datasets:
+        for mode in modes:
+            print(f"Preprocessing {mode} mode of {dataset} dataset")
             makedirs(f"{processed_dir}/{mode}", exist_ok=True)
             with open(f"{data_dir}/acousticbrainz-mediaeval-{dataset}-{mode}.tsv", 'r') as dataset_file:
                 rows = reader(dataset_file, delimiter="\t")
                 next(rows)
                 for row in rows:
-                    semaphore.acquire()
-                    p = Process(target=process_observation, args=(row, mode, dataset, sum_x, sum_x2, semaphore, output_queue))
+                    if len(pool) >= concurrent_processes:
+                        for p in pool:
+                            p.join()
+                        pool = []
+                    p = Process(target=process_observation, args=(row, mode, dataset, sum_x, sum_x2, output_queue))
                     p.start()
                     pool.append(p)
                     n += 1
@@ -107,8 +109,8 @@ def main():
         p.join()
     output_queue.put(None)
     consumer.join()
+    print("Finished first preprocessing pass")
 
-    # -- compute mean and standard deviation of every feature in order to scale them in successive steps
     means = []
     sdevs = []
     for i in range(features_dim):
@@ -120,6 +122,24 @@ def main():
         file.write(",".join(map(str, means)))
     with open(f"{processed_dir}/sdevs.csv", 'w') as file:
         file.write(",".join(map(str, sdevs)))
+    print("Calculated means and standard deviations")
+
+    print("Scale preprocessed datasets")
+    rows_to_read = 350000
+    for dataset in datasets:
+        for mode in modes:
+            file_path = f"{processed_dir}/{mode}/{dataset}"
+            print(f"Scaling {file_path}")
+            row_number = 0
+            while True:
+                try:
+                    df = pd.read_csv(f"{file_path}.csv", skiprows=row_number, nrows=rows_to_read, index_col=0, header=None)
+                except pd.errors.EmptyDataError:
+                    break
+                row_number += rows_to_read
+                df -= means
+                df /= sdevs
+                df.to_csv(f"{file_path}.clean.std.csv", mode="a", index=True, header=False)
 
 
 if __name__ == "__main__":
